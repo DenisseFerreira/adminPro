97392b5b4f4c17b7ead2886e8bc48f68925d9532
DenisseFerreira <48920693+DenisseFerreira@users.noreply.github.com>
2019-09-22 01:17:08 -0300

    Input, Output y ViewChild (#2)
    * Iniciando @Input() en componente Incrementador
    
    * Agregada libreria charts, @Input(), @Output y @ViewChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 //Modulos
 import { PagesModule } from './pages/pages.module';
+
 //Rutas
 import { AppRoutingModule } from './app-routing.module';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
 import { RegisterComponent } from './login/register.component';
+
+//temporal, debo sacarlo pronto
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
